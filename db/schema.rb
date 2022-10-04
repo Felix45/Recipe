b296d728cbff1ed2aafd7322b97bcafffca36669
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-10-04 19:25:53 +0300

    Add User Ref To Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_150636) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_161918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "preparation_time"
+    t.integer "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+    t.index ["users_id"], name: "index_recipes_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_150636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
+  add_foreign_key "recipes", "users", column: "users_id"
 end
