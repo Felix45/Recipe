81bad02f5008136fe2465aed442d2f2cfbd8ddc1
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-10-04 19:29:58 +0300

    Add Column Defaults To Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_161918) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_162748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.integer "preparation_time", default: 0
+    t.integer "cooking_time", default: 0
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
