75fdba3b9306d26bcabde33b283ddf0c01ed644d
Felix Ouma <fatonoh@gmail.com>
2022-10-05 11:41:44 +0300

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_204916) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_082624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,9 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_204916) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
-    t.index ["users_id"], name: "index_recipes_on_users_id"
   end
 
   create_table "recipes_foods", force: :cascade do |t|
@@ -65,7 +63,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_204916) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
-  add_foreign_key "recipes", "users", column: "users_id"
   add_foreign_key "recipes_foods", "foods"
   add_foreign_key "recipes_foods", "recipes"
 end
