996a6ddd22ff9daa7137bd9b5e5ecd725095b476
Felix Ouma <fatonoh@gmail.com>
2022-10-05 03:26:13 +0300

    Add user associations to User model

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :destroy, Food do |food|
+      food.user == user
+    end
+
+    can :create, Food
+  end
+end
