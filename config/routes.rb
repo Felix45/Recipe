ebcaa683d23597e3fe66bfc9fffaf4f553e35c96
Felix Ouma <fatonoh@gmail.com>
2022-10-06 00:45:53 +0300

    Add recipes_foods URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :foods
+  resources :recipes_foods
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
