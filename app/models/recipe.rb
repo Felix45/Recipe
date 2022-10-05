11e35d1dc43f15583a305efe5158cb469a93cb07
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-10-05 16:54:38 +0300

    Add Validations To Recipe Model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 end
